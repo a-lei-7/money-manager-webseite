3a50321b4e8beabfe20648a8152fa121333b23bc
liubai <errgou@gmail.com>
2020-06-04 21:39:18 +0800

    deploy

---
+++ precache-manifest.47a7e12a8795bfccfc450b5d91d92d75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da8b0f636c52385f471",
+    "url": "/money-manager-webseite/css/app.d527ddbf.css"
+  },
+  {
+    "revision": "dc8e5749bdea958ad3240d181fd9102f",
+    "url": "/money-manager-webseite/index.html"
+  },
+  {
+    "revision": "1da8b0f636c52385f471",
+    "url": "/money-manager-webseite/js/app.65133bda.js"
+  },
+  {
+    "revision": "3a0b4cdfa526044b8bf1",
+    "url": "/money-manager-webseite/js/chunk-vendors.2df6c529.js"
+  },
+  {
+    "revision": "045bea7c7a74ddd4d74358f9e3e2d047",
+    "url": "/money-manager-webseite/manifest.json"
+  },
+  {
+    "revision": "cda93cc29df4ac29c45161b8f1a933d4",
+    "url": "/money-manager-webseite/qrcode.png"
+  },
+  {
+    "revision": "5badad3d4b7c418c86772be2330298f1",
+    "url": "/money-manager-webseite/qrcode1.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-manager-webseite/robots.txt"
+  }
+]);
